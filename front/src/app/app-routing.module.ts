4004c9f542162c9aa27df768cbda80fa3460f904
Cristian Berrio <cristianberrio9864@gmail.com>
2023-05-31 00:05:53 -0500

    components of feed

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+//auth
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 
+//feed
+import { FeedComponent } from './components/feed/feed.component';
+import { PostsComponent } from './components/feed/posts/posts.component';
+import { CreatePostComponent } from './components/feed/create-post/create-post.component';
+import { MyPostsComponent } from './components/feed/my-posts/my-posts.component';
+
 const routes: Routes = [
-  { path: "auth/login", component: LoginComponent },
-  { path: "auth/register", component: RegisterComponent }
+  { path: "auth/login", component: LoginComponent, title: "Login" },
+  { path: "auth/register", component: RegisterComponent, title: "Register" },
+  { path: "", redirectTo: 'auth/login', pathMatch: "full" },
+  {
+    path: "home", component: FeedComponent, children: [
+      { path: "", component: PostsComponent },
+      { path: "create-post", component: CreatePostComponent },
+      { path: "my-posts", component: MyPostsComponent }
+    ]
+  }
 ];
 
 @NgModule({
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -6,12 +6,22 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CreatePostComponent } from './components/feed/create-post/create-post.component';
+import { MyPostsComponent } from './components/feed/my-posts/my-posts.component';
+import { PostsComponent } from './components/feed/posts/posts.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FeedComponent } from './components/feed/feed.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    CreatePostComponent,
+    MyPostsComponent,
+    PostsComponent,
+    NavbarComponent,
+    FeedComponent
   ],
   imports: [
     BrowserModule,
