302034b709e6ec7df98d276e6b521959b6a4079c
Cristian Berrio <cristianberrio9864@gmail.com>
2023-05-31 09:37:59 -0500

    my posts

--- front/src/app/components/feed/my-posts/my-posts.component.ts
+++ front/src/app/components/feed/my-posts/my-posts.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
+
+import { faPaperPlane, faAnglesLeft, faAngleLeft, faAnglesRight, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-my-posts',
   templateUrl: './my-posts.component.html',
   styleUrls: ['./my-posts.component.scss']
 })
-export class MyPostsComponent {
 
+export class MyPostsComponent {
+  filterForm!: FormGroup;
+  fapaperplane = faPaperPlane;
+  faanglesleft = faAnglesLeft;
+  faangleleft = faAngleLeft;
+  faanglesright = faAnglesRight;
+  faangleright = faAngleRight;
+
+  constructor(private fb: FormBuilder) {
+
+  }
+
+  ngOnInit() {
+    this.filterForm = this.fb.group({
+      date: ['']
+   }); 
+  }
+
+  filter() {
+    
+  }
 }
